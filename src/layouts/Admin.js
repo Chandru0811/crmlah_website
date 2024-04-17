0c37076ecb21f05bf6b2f02f1e32c13d162d3b07
keerthick07 <keerthickvasan08@gmail.com>
2024-04-17 12:56:04 +0530

    About page

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -4,6 +4,7 @@ import Footer from "../Components/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Contact from "../Components/Contact";
 import Home from "../Components/Home";
+import About from "../Components/About";
 // import About from "../Components/About";
 // import Feature from "../Components/Feature";
 // import Contact from "../Components/Contact";
@@ -14,8 +15,8 @@ function Admin() {
       <BrowserRouter>
         <Header />
         <Routes>
-           {/* <Route path="/about" element={<About />} />
-          <Route path="/features" element={<Feature />} /> */}
+           <Route path="/about" element={<About />} />
+          {/* <Route path="/features" element={<Feature />} /> */}
           <Route path="/home" element={<Home />} /> 
           <Route path="/contact" element={<Contact />} /> 
         </Routes>
